594089a36235bea05bb2ac85e688f1d8077f6c72
luca.rossi <luxx1991@gmail.com>
2018-06-18 10:37:55 +0200

    fix bollettino block

--- sites/all/modules/custom_ccmmma/home/src/Plugin/Block/bollettinoBlock.php
+++ sites/all/modules/custom_ccmmma/home/src/Plugin/Block/bollettinoBlock.php
@@ -15,131 +15,160 @@ use GuzzleHttp\Exception\RequestException;
  * )
  */
 class bollettinoBlock extends BlockBase {
+
   /**
    * {@inheritdoc}
    */
   public function build() {
-    
+
     \Drupal::service('page_cache_kill_switch')->trigger();
     //$build['#cache']['max-age'] = 0;
     $api = \Drupal::config('api.settings')->get('api');
-    $url_napoli = $api.'/products/wrf5/timeseries/com63049?step=24&date='.date('Ymd\Z\0\0', time());
-    
+    $url_napoli = $api . '/products/wrf5/timeseries/com63049?step=24&date=' . date('Ymd\Z\0\0', time());
+
     //creo un array con 6 date a partire da oggi
-    $list_of_day = array();
-    $list_of_result = array();
-    $days = array('Domenica', 'Lunedì', 'Martedì', 'Mercoledì','Giovedì','Venerdì', 'Sabato');
-    
-    $list_of_day[date('Ymd\Z\0\000', time())] = $days[date('w', time())].' '.date('d', time());//oggi
+    $list_of_day = [];
+    $list_of_result = [];
+    $days = [
+      'Domenica',
+      'Lunedì',
+      'Martedì',
+      'Mercoledì',
+      'Giovedì',
+      'Venerdì',
+      'Sabato',
+    ];
+
+    $list_of_day[date('Ymd\Z\0\000', time())] = $days[date('w', time())] . ' ' . date('d', time());//oggi
     $i = 1;
-    for($i; $i<=7; $i++){
+    for ($i; $i <= 7; $i++) {
       //$list_of_day[gmdate('Ymd\Zhi', time()+(86400*$i))] = gmdate('w', time()+(86400*$i));
-      $list_of_day[date('Ymd\Z\0\000', time()+(86400*$i))] = $days[date('w', time()+(86400*$i))].' '.date('d', time()+(86400*$i)); //oggi
+      $list_of_day[date('Ymd\Z\0\000', time() + (86400 * $i))] = $days[date('w', time() + (86400 * $i))] . ' ' . date('d', time() + (86400 * $i)); //oggi
     }
     //dpm($list_of_day);
     //get result of api
     $client = new \GuzzleHttp\Client();
-    try{
-	    $request = $client->get($url_napoli, ['http_errors' => false]);
-	    //dpm($url_napoli);
-	    //dpm($list_of_day);
-	    $response = json_decode($request->getBody());
-	    if(isset($response->timeseries)){
-		    foreach($response->timeseries as $single_value){
-				if(array_key_exists($single_value->dateTime, $list_of_day)){
-					$list_of_result[$single_value->dateTime] = $single_value;
-		  		}
-			}
-    	}
-    } catch(RequestException $e){
-	    $list_of_result = [];
+    try {
+      $request = $client->get($url_napoli, ['http_errors' => FALSE]);
+      //dpm($url_napoli);
+      //dpm($list_of_day);
+      $response = json_decode($request->getBody());
+      if (isset($response->timeseries)) {
+        foreach ($response->timeseries as $single_value) {
+          if (array_key_exists($single_value->dateTime, $list_of_day)) {
+            $list_of_result[$single_value->dateTime] = $single_value;
+          }
+        }
+      }
+    } catch (RequestException $e) {
+      $list_of_result = [];
     }
 
 
-    
-    
     //dpm($list_of_result);
     global $base_url;
-    $path_publich = $base_url.'/sites/all/themes/zircon_custom/js/images/';
-    
+    $path_publich = $base_url . '/sites/all/themes/zircon_custom/js/images/';
+
     $markup = '';
-   
+
+    $data = [];
+
     //dpm($list_of_result);
     //dpm($list_of_day);
-    foreach($list_of_day as $time => $string){
+
+    $markup .= '    <div id="box">  <div class="title">Meteo Comune di Napoli    <a href="http://meteo.uniparthenope.it" target="_blank" title="meteo.uniparthenope.it">    </a>  </div><div id="oBox_loading" style="display: none;">    <img src="http://meteo.uniparthenope.it/sites/default/files/animated_progress_bar.gif" width="400"></div>';
+
+    foreach ($list_of_day as $time => $string) {
       //dpm($time);
-      if(isset($list_of_result[$time])){
+      if (isset($list_of_result[$time])) {
         $result_array = get_object_vars($list_of_result[$time]);
+        dpm($result_array);
         //dpm($result_array);
-        if(isset($result_array['icon'])){
-          $icon = $result_array['icon'];
-          $pos = strpos($icon, '_night');
-          if($pos){
-            $icon = str_replace('_night', '', $icon);
+        if (isset($result_array['icon'])) {
+          $data[$string]['icon'] = $result_array['icon'];
+          $pos = strpos($data[$string]['icon'], '_night');
+          if ($pos) {
+            $data[$string]['icon'] = str_replace('_night', '', $data[$string]['icon']);
           }
-        } else{
-          $icon = '';
         }
-        if(isset($result_array['icon'])){
-          $meteo_text = $result_array['text'];
-        } else{
-          $meteo_text = '';
+        else {
+          $data[$string]['icon'] = '';
         }
-        
-        if(isset($result_array['wd10'])){
-          $vento_type = $result_array['wd10']->value;
-        } else{
-          $vento_type = '';
-        } 
-        if(isset($result_array['ws10-max'])){
-          $vento_max = $result_array['ws10-max']->value;
-        } else{
-          $vento_max = 0;
+        if (isset($result_array['text'])) {
+          $data[$string]['meteo_text'] = $result_array['text'];
         }
-        if(isset($result_array['ws10-min'])){
-          $vento_min = $result_array['ws10-min']->value;
-        } else{
-          $vento_min = 0;
+        else {
+          $data[$string]['meteo_text'] = '';
         }
-        if($vento_max == 0 && $vento_min == 0){
-	        if(isset($result_array['ws10'])){
-				$vento = $result_array['ws10']->value;
-        	} else{
-				$vento = 0;
-        	}
-        } else{
-        	$vento = round((($vento_max + $vento_min) /2) * 1.9, 0); //conversione in nodi
+        if (isset($result_array['wd10'])) {
+          $data[$string]['vento_type'] = $result_array['wd10'];
         }
-        
-        if(isset($result_array['crh'])){
-          $pioggia = round($result_array['crh']->value, 1);
-        } else{
-          $pioggia = 0;
+        else {
+          $data[$string]['vento_type'] = '';
         }
-        if(isset($result_array['t2c-min'])){
-          $temp_min = round($result_array['t2c-min']->value, 0);
-        } else{
-          $temp_min = 0;
+        if (isset($result_array['ws10-max'])) {
+          $data[$string]['vento_max'] = $result_array['ws10-max'];
         }
-        if(isset($result_array['t2c-max'])){
-          $temp_max = round($result_array['t2c-max']->value, 0);
-        } else{
-          $temp_max = 0;
+        else {
+          $data[$string]['vento_max'] = 0;
         }
-        if(isset($result_array['t2c'])){
-          $temp = round($result_array['t2c']->value, 0);
-        } else{
-          $temp = 0;
+        if (isset($result_array['ws10-min'])) {
+          $data[$string]['vento_min'] = $result_array['ws10-min'];
         }
+        else {
+          $data[$string]['vento_min'] = 0;
+        }
+        if ($data[$string]['vento_min'] == 0 && $data[$string]['vento_max'] == 0) {
+          if (isset($result_array['ws10'])) {
+            $data[$string]['vento'] = $result_array['ws10'];
+          }
+          else {
+            $data[$string]['vento'] = 0;
+          }
+        }
+        else {
+          $data[$string]['vento'] = round((($data[$string]['vento_max'] + $data[$string]['vento_min']) / 2) * 1.9, 0); //conversione in nodi
+        }
+
+        if (isset($result_array['crh'])) {
+          $data[$string]['pioggia'] = round($result_array['crh'], 1);
+        }
+        else {
+          $data[$string]['pioggia'] = 0;
+        }
+        if (isset($result_array['t2c-min'])) {
+          $data[$string]['temp_min'] = round($result_array['t2c-min'], 0);
+        }
+        else {
+          $data[$string]['temp_min'] = 0;
+        }
+        if (isset($result_array['t2c-max'])) {
+          $data[$string]['vento_max'] = round($result_array['t2c-max'], 0);
+        }
+        else {
+          $data[$string]['vento_max'] = 0;
+        }
+        if (isset($result_array['t2c'])) {
+          $data[$string]['temp'] = round($result_array['t2c'], 0);
+        }
+        else {
+          $data[$string]['temp'] = 0;
+        }
+
 
         //caso in cui ho solo una temperatura
-        if($temp_min == 0 && $temp_max == 0){
-	        $markup .=' 
-    
-    <div id="box">  <div class="title">Meteo Comune di Napoli    <a href="http://meteo.uniparthenope.it" target="_blank" title="meteo.uniparthenope.it">    </a>  </div><div id="oBox_loading" style="display: none;">    <img src="http://meteo.uniparthenope.it/sites/default/files/animated_progress_bar.gif" width="400"></div>
-
+        if ($data[$string]['temp_min'] == 0 && $data[$string]['temp_max'] == 0) {
+          $single_temp_value = 1;
+        }
+        else {
+          $single_temp_value = 0;
+        }
+      }
+    }
+    //creazione intestazione tabella
+    if ($single_temp_value) {
+      $markup .= ' 
     <table id="oBox_table" width="100%" cellspacing="0" cellpadding="2" border="0" style="">
-
         <tbody>
           <tr class="legenda">
             <td width="24%" colspan="2">Previsione</td>
@@ -147,19 +176,10 @@ class bollettinoBlock extends BlockBase {
             <td width="21%" colspan="2">Vento</td>
             <td width="14%">Pioggia</td>
           </tr>';
-
-	        $markup .= '
-				<tr>
-					<td class="data">'.$string.'</td><td class="data"><img src="'.$path_publich.$icon.'" width="16&" height="16&" alt="'.$meteo_text.'" title="'.$meteo_text.'"></td>  <td class="data tmin">'.$temp.'°C</td><td class="data">'.$vento_type.'</td>  <td class="data">'.$vento.' knt</td><td class="data">'.$pioggia.' mm</td> 
-				</tr>';
-        } else{
-	        
-	        $markup .=' 
-    
-    <div id="box">  <div class="title">Meteo Comune di Napoli    <a href="http://meteo.uniparthenope.it" target="_blank" title="meteo.uniparthenope.it">    </a>  </div><div id="oBox_loading" style="display: none;">    <img src="http://meteo.uniparthenope.it/sites/default/files/animated_progress_bar.gif" width="400"></div>
-
+    }
+    else {
+      $markup .= ' 
     <table id="oBox_table" width="100%" cellspacing="0" cellpadding="2" border="0" style="">
-
         <tbody>
           <tr class="legenda">
             <td width="24%" colspan="2">Previsione</td>
@@ -168,28 +188,36 @@ class bollettinoBlock extends BlockBase {
             <td width="21%" colspan="2">Vento</td>
             <td width="14%">Pioggia</td>
           </tr>';
+    }
 
-        
+    //creazione parte dinamica della tabella
+    foreach($data as $string => $value){
+      if($single_temp_value){
+        $markup .= '
+				<tr>
+					<td class="data">' . $string . '</td><td class="data"><img src="' . $path_publich . $value['icon'] . '" width="16&" height="16&" alt="' . $value['meteo_text'] . '" title="' . $value['meteo_text'] . '"></td>  <td class="data tmin">' . $value['temp'] . '°C</td><td class="data">' . $value['vento_type'] . '</td>  <td class="data">' . $value['vento'] . ' knt</td><td class="data">' . $value['pioggia'] . ' mm</td> 
+				</tr>';
+      } else{
         $markup .= '
         <tr>
-          <td class="data">'.$string.'</td><td class="data"><img src="'.$path_publich.$icon.'" width="16&" height="16&" alt="'.$meteo_text.'" title="'.$meteo_text.'"></td>  <td class="data tmin">'.$temp_min.'°C</td>  <td class="data tmax">'.$temp_max.'°C</td>  <td class="data">'.$vento_type.'</td>  <td class="data">'.$vento.' knt</td><td class="data">'.$pioggia.' mm</td> 
+          <td class="data">' . $string . '</td><td class="data"><img src="' . $path_publich . $value['icon'] . '" width="16&" height="16&" alt="' . $value['meteo_text'] . '" title="' . $value['meteo_text'] . '"></td>  <td class="data tmin">' . $value['temp_min'] . '°C</td>  <td class="data tmax">' . $value['temp_max'] . '°C</td>  <td class="data">' . $value['vento_type'] . '</td>  <td class="data">' . $value['vento'] . ' knt</td><td class="data">' . $value['pioggia'] . ' mm</td> 
         </tr>
         ';
       }
-    }  
-    }     
-      $markup .= '
+    }
+    //chiusura della tabella
+    $markup .= '
         </tbody>
     </table>
     <div class="meteo.ink">  <a href="http://meteo.uniparthenope.it" target="_blank" title="Meteo">    CCMMMA: http://meteo.uniparthenope.it  </a>  <br>  ©2013  <a href="http://meteo.uniparthenope.it/" title="Meteo siti web" target="_blank">    <b>meteo.uniparthenope.it</b> - <b>CCMMMA</b> Università Parthenope  </a>  </div></div>';
 
-    return array(
+    return [
       '#markup' => \Drupal\Core\Render\Markup::create($markup),
-      '#cache' => array(
+      '#cache' => [
         'max-age' => 0,
-      ),
+      ],
       //TODO add cache manage
-    );
+    ];
   }
 
 }
