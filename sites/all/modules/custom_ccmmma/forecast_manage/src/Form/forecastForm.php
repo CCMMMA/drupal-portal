28fedbbfccffed8dd07990c932d17081e6bc3f6d
C.C.M.M.M.A <ccmmma@uniparthenope.it>
2018-11-23 18:33:39 +0100

    sistemazione layout forecast immagine + scale

--- sites/all/modules/custom_ccmmma/forecast_manage/src/Form/forecastForm.php
+++ sites/all/modules/custom_ccmmma/forecast_manage/src/Form/forecastForm.php
@@ -252,21 +252,22 @@ class forecastForm extends FormBase {
     if(isset($response->map->link)){
       $link_map = $response->map->link;
     }
-    $markup_legend_left = '<div class="col-lg-2"><img class="legend-left" src="https://api.meteo.uniparthenope.it/products/'.$prod.'/forecast/legend/left/gen?width=64&height=563&date='.$date.'"></div>';
-    $markup_legend_right = '<div class="col-lg-2"><img class="legend-right" src="https://api.meteo.uniparthenope.it/products/'.$prod.'/forecast/legend/right/gen?width=64&height=563&date='.$date.'"></div>';
-    $markup_legend_bottom = '<div class="col-lg-8 col-lg-offset-2"><img class="legend-bottom" src="https://api.meteo.uniparthenope.it/products/'.$prod.'/forecast/legend/bottom/gen?width=768&height=64&date='.$date.'"></div>';
+    $markup_legend_left = '<div class="col-lg-2 col-xs-2"><img class="legend-left" src="https://api.meteo.uniparthenope.it/products/'.$prod.'/forecast/legend/left/gen?width=64&height=563&date='.$date.'"></div>';
+    $markup_legend_right = '<div class="col-lg-2 col-xs-2"><img class="legend-right" src="https://api.meteo.uniparthenope.it/products/'.$prod.'/forecast/legend/right/gen?width=64&height=563&date='.$date.'"></div>';
+    $markup_legend_bottom = '<div class="col-lg-8 col-xs-8 col-lg-offset-2 col-xs-offset-2"><img class="legend-bottom" src="https://api.meteo.uniparthenope.it/products/'.$prod.'/forecast/legend/bottom/gen?width=768&height=64&date='.$date.'"></div>';
 
     //dpm('link alla mappa: '.$link_map);
     if($link_map === NULL){
       $img_result = '<p>Impossibile caricare immagine</p>';
     }
     else{
-      $img_result = '<div class="col-lg-8"><img class="img-forecast" src="'.$link_map.'"></div>';
+      $img_result = '<div class="col-lg-8 col-xs-8"><img class="img-forecast" src="'.$link_map.'"></div>';
     }
 
-    $markup_image = '<div class="row">';
-    $markup_image .= $markup_legend_left . $img_result . $markup_legend_right . $markup_legend_bottom;
-    $markup_image .= '</div>';
+    $markup_image = '<div class="container-fluid"><div class="row">';
+    $markup_image .= $markup_legend_left . $img_result . $markup_legend_right;
+    $markup_image .= '</div><div class="row">'.$markup_legend_bottom;
+    $markup_image .= '</div></div>';
 
 
 
